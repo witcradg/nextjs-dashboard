b176eb068a2e6ced6a065cd6380188cf8a9ae4ed
witcradg <witcradg@gmail.com>
2023-11-25 10:44:09 -0800

    Clean up comment token

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -20,7 +20,7 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
+        <Card title="Collected" value={totalPaidInvoices} type="collected" /> 
         <Card title="Pending" value={totalPendingInvoices} type="pending" />
         <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
         <Card
